5b747344c9399cc4a853f2a75ec1625ced88fa9e
Promomotionbot <86830116+Promomotionbot@users.noreply.github.com>
2021-08-25 10:18:52 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -6,18 +6,23 @@ class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
 
-<code>I am a small open source project , Add me into your group and enjoy. </code>
+<code>I am an Advanced Autofilter Bot, Add me into your group and enjoy. </code>
 
 Click /help for more informations. 
 """    
     
     HELP_TEXT = """
 => Add Bot To Any Channel As Admin With Add Members/ Invite Users Via Link
+
 => Copy Channel ID
+
 => Use <code>/connect {channel id}</code> In Your Group To Connect With The Group
+
 => Use <code>/disconnect {channel id}</code> In Your Group To Disconnect From Your Group
+
 => Use <code>/delall</code> In Your Group To Clear All Your Group  Connections (Owner Only)
 Now You Are All Set And Ready To Go...
+
 Just Send Any Text Will Try To Lookup In Channel And Provide You The Link.
 """
     
